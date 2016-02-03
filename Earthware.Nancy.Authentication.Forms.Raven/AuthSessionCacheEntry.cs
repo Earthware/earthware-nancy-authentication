7dd51e1fa84f060431a63528ed9269b02216604b
Jonathan George <jon@jonathangeorge.co.uk>
2016-02-03 13:08:09 +0000

    Added RavenDb implementation of IAuthSessionIdStore

---
+++ Earthware.Nancy.Authentication.Forms.Raven/AuthSessionCacheEntry.cs
@@ -0,0 +1,23 @@
+﻿namespace Earthware.Nancy.Authentication.Forms.Raven
+{
+    using System;
+
+    public class AuthSessionCacheEntry
+    {
+        public AuthSessionCacheEntry()
+        {
+            this.Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public DateTime ExpiryTimeUtc { get; set; }
+
+        public bool IsValid()
+        {
+            return this.ExpiryTimeUtc > DateTime.UtcNow;
+        }
+    }
+}
---
+++ Earthware.Nancy.Authentication.Forms.Raven/RavenAuthSessionIdStore.cs
@@ -0,0 +1,62 @@
+﻿namespace Earthware.Nancy.Authentication.Forms.Raven
+{
+    using System;
+
+    using global::Raven.Client;
+
+    public class RavenAuthSessionIdStore : IAuthSessionIdStore
+    {
+        private readonly IDocumentSession session;
+
+        public RavenAuthSessionIdStore(IDocumentSession session)
+        {
+            this.session = session;
+        }
+
+        public Guid Add(Guid persistentUserIdentifier, DateTime expiryTimeUtc)
+        {
+            var doc = new AuthSessionCacheEntry { UserId = persistentUserIdentifier, ExpiryTimeUtc = expiryTimeUtc };
+            this.session.Store(doc);
+
+            return doc.Id;
+        }
+
+        public Guid Get(Guid authSessionIdentifier)
+        {
+            var doc = this.session.Load<AuthSessionCacheEntry>(authSessionIdentifier);
+
+            if (doc == null)
+            {
+                return Guid.Empty;
+            }
+
+            if (doc.IsValid())
+            {
+                return doc.UserId;
+            }
+
+            this.session.Delete(doc);
+            return Guid.Empty;
+        }
+
+        public void Extend(Guid authSessionIdentifier, DateTime expiryTimeUtc)
+        {
+            var doc = this.session.Load<AuthSessionCacheEntry>(authSessionIdentifier);
+
+            if (doc != null)
+            {
+                doc.ExpiryTimeUtc = expiryTimeUtc;
+            }
+        }
+
+        public void Remove(Guid authSessionIdentifier)
+        {
+            var doc = this.session.Load<AuthSessionCacheEntry>(authSessionIdentifier);
+
+            if (doc != null)
+            {
+                this.session.Delete(doc);
+            }
+        }
+    }
+}
